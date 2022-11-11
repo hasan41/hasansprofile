ebe252f7e8f6a9a143700489544c8896c24a5c9e
hasan41 <hasan.naseer1470@gmail.com>
2022-11-11 01:31:27 -0500

    Changed my face pic

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,8 @@ export const headerData = {
     name: 'Hasan Naseer',
     title: "Hello! I am",
     desciption: "Hello World! Hasan, is a 22 year old full-stack Software Engineer and a YouTuber. He recently completed his BS from Georgia Institute of Technology in Computer Science (AI). He is tech enthusiastic and passionate about Machine Learning, Deep Learning, and transforming different neural networks architecture. ",
-    image: 'https://i.ibb.co/MPhB2xh/hasan.jpg',
+    image: 'https://i.ibb.co/b1f61mM/IMG-1233-2.jpg',
+    // image: 'https://i.ibb.co/MPhB2xh/hasan.jpg'
     //https://i.ibb.co/qnG4L7n/IMG-1097.jpg
     resumePdf: 'https://drive.google.com/file/d/1MfqeCbrSapGe7rhh4M8-Y4ZTL5Q1D9si/view?usp=sharing'
 }
